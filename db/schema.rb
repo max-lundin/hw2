258e10e007f8867283bc7610ae28e1f0bf9c856a
max lundin <max.lundin@kellogg.northwestern.edu>
2024-02-06 05:42:59 +0000

    created movies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_053559) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_053756) do
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year_released"
+    t.string "rated"
+    t.integer "studio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "studios", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
